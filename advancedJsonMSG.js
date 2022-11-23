5ccd0e5aab7f7b377835a43345f98c93784aa800
takeabyte <takeabyte@users.noreply.github.com>
2022-11-23 22:45:48 +0100

    Create advancedJsonMSG.js

---
+++ advancedJsonMSG.js
@@ -0,0 +1,12 @@
+{
+{{#decoded}}{{#payload}}
+"temperature": {{temperature}},
+"humidity": {{humidity}},
+"pressure": {{pressure}},
+"gas": {{gas}},
+"light": 0,
+"battery": {{battery}},
+{{/payload}}{{/decoded}}
+"name": "{{name}}",
+"reported_at": {{reported_at}}
+}
